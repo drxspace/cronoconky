08af760c4e8e7bb4dab95ce3b45d9e7b4fae2957
John A Ginis <drxspace@gmail.com>
2013-10-21 10:06:53 +0300

    leading spaces

--- cronograph_se/start_crono.sh
+++ cronograph_se/start_crono.sh
@@ -12,7 +12,7 @@
 #      ^^
 # Error of failed request:  BadWindow (invalid Window parameter)
 # There's also the X-GNOME-Autostart-Delay=25 property in .desktop file
-# that can be set in order tohandle this situation
+# that can be set in order to handle this situation
 #rm "$(dirname "$0")"/conkyerr.log && $(which conky) -DD -c "$(dirname "$0")"/cronorc &> "$(dirname "$0")"/conkyerr.log &
 
 #conky -q -c "$(dirname "$0")"/cronorc &
