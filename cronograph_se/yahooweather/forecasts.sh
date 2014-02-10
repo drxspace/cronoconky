eff4d96d49533ba49baef7fcf3940ce3c86fd78b
John A Ginis <drxspace@gmail.com>
2014-02-10 21:35:27 +0200

    yahoo!

---
+++ cronograph_se/yahooweather/forecasts.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+#
+# _________        ____  ____________         _______ ___________________
+# ______  /__________  |/ /___  ____/________ ___    |__  ____/___  ____/
+# _  __  / __  ___/__    / ______ \  ___  __ \__  /| |_  /     __  __/
+# / /_/ /  _  /    _    |   ____/ /  __  /_/ /_  ___ |/ /___   _  /___
+# \__,_/   /_/     /_/|_|  /_____/   _  .___/ /_/  |_|\____/   /_____/
+#                                    /_/           drxspace@gmail.com
+#
+
+# getImgChr () function converts
+# YAHOO! weather icon codes (http://developer.yahoo.com/weather/#codes)
+# to characters for use with the ConkyWeather truetype fonts
+getImgChr () {
+	case $1 in
+		0) # sunny
+			echo a
+		;;
+		1) # sunny
+			echo a
+		;;
+		2) # mostly sunny
+			echo b
+		;;
+		3|4|5) # partly sunny | intermittent clouds | hazy sunshine
+			echo c
+		;;
+		6) # mostly cloudy
+			echo d
+		;;
+		7) # cloudy
+			echo e
+		;;
+		8) # dreary
+			echo f
+		;;
+		9) # dreary
+			echo f
+		;;
+		10) # dreary
+			echo f
+		;;
+		11) # fog
+			echo 0
+		;;
+		12) # showers
+			echo h
+		;;
+		13|14) # mostly cloudy with showers | partly sunny with showers
+			echo g
+		;;
+		15) # thunderstorms
+			echo l
+		;;
+		16|17) # mostly clooudy with thunderstorms | partly sunny with thundershowers
+			echo k
+		;;
+		18) # rain
+			echo i
+		;;
+		19) # flurries
+			echo q
+		;;
+		20|21|23) # mostly cloudy with flurries | partly sunny with flurries | mostly cloudy with snow
+			echo o
+		;;
+		22) # snow
+			echo r
+		;;
+		24|31) # ice | cold
+			echo E
+		;;
+		25) # sleet
+			echo v
+		;;
+		26) # freezing rain
+			echo x
+		;;
+		# icons 27-28 have been retired
+		29) # rain and snow
+			echo y
+		;;
+		30) # hot
+			echo 5
+		;;
+		32) # windy
+			echo 6
+		;;
+		33) # clear
+			echo A
+		;;
+		34|35) # mostly clear | partly cloudy
+			echo B
+		;;
+		36|37) # intermittent clouds | hazy
+			echo C
+		;;
+		38) # mostly cloudy
+			echo D
+		;;
+		39|40) # partly cloudy with showers | mostly cloudy with showers
+			echo G
+		;;
+		41|42) # partly cloudy with thunder showers | mostly cloudy with thunder showers
+			echo K
+		;;
+		43|44) # mostly cloudy with flurries | mostly cloudy with snow
+			echo O
+		;;
+		*)
+			echo -
+		;;
+	esac
+}
+
+# User Agent String from http://www.useragentstring.com
+# Suppose we're using Chrome/30.0.1599.17
+UserAgent='Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36'
+
+# The directory this script resides
+scriptDir="$(dirname "$0")"
+
+# Store temporary data in this directory...
+cacheDir="$HOME/.cache/cronograph"
+# ...but first make sure that it's clear
+[[ -d "${cacheDir}" ]] && rm -f "${cacheDir}"/* || mkdir -p "${cacheDir}"
+
+# Store temporary RSS Feed in this file
+cacheFile="YahooWeather.xml"
+
+# clearconds () function clears the contents of conditions files
+clearconds () {
+	cat /dev/null > "${scriptDir}"/curr_cond
+	cat /dev/null > "${scriptDir}"/fore_cond
+}
+
+## parseval () function parses the value of the specified tag
+#parseval () {
+#	sed -n "s|<$1>\(.*\)</$1>|\1|p" $2 | sed "s/^[[:space:]]*//"
+#}
+
+# trimday () function returns a 3 letters word for the given day name
+trimday () {
+	local day=${1^^}
+	echo ${day:0:3}
+}
+
+# errexit () function clears the cond files so that nothing would be displayed on
+# the clock, writes an error on the stderr file and then exits the script
+errexit () {
+	echo -e "$1" >&2
+	clearconds
+	echo "Yahoo! Error..." > "${scriptDir}"/curr_cond
+	exit 1
+}
+
+###
+#
+# main section
+#
+
+# INFO: Navigate to http://weather.yahoo.com/ enter your or zip code to locate
+#       your place you want to watch and get the WOEID number at url's end e.g.
+#       http://weather.yahoo.com/greece/attica/athens-946738/
+#                                                     ^^^^^^
+#       This is the WOEID (where on Earth ID) number we're talking.
+#       Then replace the following one with the WOEID of your own location.
+WOEID='946738'
+
+# Uncomment next line to make use of English units
+#DegreesUnits='f'
+
+# Yahoo Weather RSS Feed url
+YahooWurl="http://weather.yahooapis.com/forecastrss?w=${WOEID}&u=${DegreesUnits:-c}"
+
+echo -e "forecasts.sh: Contacting the server at url:\n\t${YahooWurl}" >&2
+
+wget -q -4 -t 1 --user-agent="${UserAgent}" -O "${cacheDir}"/"${cacheFile}" "${YahooWurl}" ||
+	errexit "ERROR: Wget exits with error code $?."
+
+echo "forecasts.sh: Checking the results..." >&2
+
+[[ -z $(grep "yweather:condition" "${cacheDir}"/"${cacheFile}") ]] &&
+       errexit "ERROR: Yahoo Weather server reports failure."
+
+echo "forecasts.sh: Processing data..." >&2
+
+pkill -SIGSTOP --oldest --exact --full "^conky.*cronorc$"
+
+echo "$(grep "yweather:condition" "${cacheDir}"/"${cacheFile}" | grep -o "temp=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*")"° > "${scriptDir}"/curr_cond
+getImgChr $(grep "yweather:condition" "${cacheDir}"/"${cacheFile}" | grep -o "code=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*") >> "${scriptDir}"/curr_cond
+grep "yweather:condition" "${cacheDir}"/"${cacheFile}" | grep -o "text=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" >> "${scriptDir}"/curr_cond
+
+getImgChr $(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "code=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==1' | tr '[a-z]' '[A-Z]') > "${scriptDir}"/fore_cond
+getImgChr $(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "code=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==2' | tr '[a-z]' '[A-Z]') >> "${scriptDir}"/fore_cond
+getImgChr $(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "code=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==3' | tr '[a-z]' '[A-Z]') >> "${scriptDir}"/fore_cond
+
+echo "$(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "low=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==2' | tr '[a-z]' '[A-Z]')°/\
+$(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "high=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==2' | tr '[a-z]' '[A-Z')°" >> "${scriptDir}"/fore_cond
+echo "$(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "low=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==3' | tr '[a-z]' '[A-Z]')°/\
+$(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "high=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==3' | tr '[a-z]' '[A-Z]')°" >> "${scriptDir}"/fore_cond
+echo "$(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "low=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==4' | tr '[a-z]' '[A-Z]')°/\
+$(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "high=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==4' | tr '[a-z]' '[A-Z]')°" >> "${scriptDir}"/fore_cond
+
+trimday $(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "day=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==2' | tr '[a-z]' '[A-Z]') >> "${scriptDir}"/fore_cond
+trimday $(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "day=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==3' | tr '[a-z]' '[A-Z]') >> "${scriptDir}"/fore_cond
+trimday $(grep "yweather:forecast" "${cacheDir}"/"${cacheFile}" | grep -o "day=\"[^\"]*\"" | grep -o "\"[^\"]*\"" | grep -o "[^\"]*" | awk 'NR==4' | tr '[a-z]' '[A-Z]') >> "${scriptDir}"/fore_cond
+
+pkill -SIGCONT --oldest --exact --full "^conky.*cronorc$"
+
+exit 0
